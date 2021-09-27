133d7a08aae38a68437940aa0a5da7510ea177ab
miatang13 <miatang13@gmail.com>
2021-09-27 16:52:34 -0400

    core: add react p5 wrapper

---
+++ ccl-data-vis/src/components/p5-map.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Sketch from "react-p5";
+
+function P5map() {
+  console.log("hi");
+  const setup = (p5, canvasParentRef) => {
+    console.log(canvasParentRef);
+    p5.createCanvas(500, 400).parent(canvasParentRef);
+  };
+
+  const draw = (p5) => {
+    p5.background(255, 130, 20);
+    p5.ellipse(100, 100, 100);
+    p5.ellipse(300, 100, 100);
+  };
+
+  return <Sketch setup={setup} draw={draw} />;
+}
+
+export default P5map;
