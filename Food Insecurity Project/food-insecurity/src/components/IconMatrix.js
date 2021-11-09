7aafb56f2ba2138ebef14aa8d16514b6ec44f89b
miatang13 <miatang13@gmail.com>
2021-11-09 18:47:38 -0500

    Working with Overlay comp

--- Food Insecurity Project/food-insecurity/src/components/IconMatrix.js
+++ Food Insecurity Project/food-insecurity/src/components/IconMatrix.js
@@ -1,9 +1,58 @@
-import testIcon from "../assets/img/matrixIcon/test.png";
+import Button from "@restart/ui/esm/Button";
+import Overlay from "@restart/ui/esm/Overlay";
+import { useRef, useState } from "react";
+import { OverlayTrigger } from "react-bootstrap";
 
-export default function IconMatrix(props) {
+export default function IconMatrix(iconProp) {
+  /*
+  const target = useRef(null);
+  const [show, setShow] = useState(false);
+
+  const renderInfo = () => {
+    <div className="icon__info">
+      <span>{props.data.name} </span>
+      <span>{props.data.location} </span>
+      <p> {props.data.blurb} </p>
+    </div>;
+  };
+
   return (
-    <div>
-      <img className="matrix__icon__img" src={testIcon} alt="icon" />
-    </div>
+    <OverlayTrigger
+      placement="right"
+      delay={{ show: 250, hide: 400 }}
+      overlay={renderInfo}
+    >
+      <div className="individual__icon" ref={target}>
+        <Button variant="success">Hover me to see</Button>
+      </div>
+    </OverlayTrigger>
+  );*/
+  const [show, setShow] = useState(false);
+  const target = useRef(null);
+
+  return (
+    <>
+      <Button variant="danger" ref={target} onClick={() => setShow(!show)}>
+        Click me to see
+      </Button>
+      <Overlay target={target.current} show={show} placement="right">
+        {({ placement, arrowProps, show: _show, popper, ...props }) => (
+          <div
+            {...props}
+            style={{
+              backgroundColor: "rgba(255, 100, 100, 0.85)",
+              padding: "2px 10px",
+              color: "white",
+              borderRadius: 3,
+              ...props.style,
+            }}
+          >
+            <span>{iconProp.data.name} </span>
+            <span>{iconProp.data.location} </span>
+            <p> {iconProp.data.blurb} </p>
+          </div>
+        )}
+      </Overlay>
+    </>
   );
 }
--- Food Insecurity Project/food-insecurity/src/components/MatrixComp.js
+++ Food Insecurity Project/food-insecurity/src/components/MatrixComp.js
@@ -1,10 +1,18 @@
 import IconMatrix from "./IconMatrix";
+import testIcon from "../assets/img/matrixIcon/test.png";
 
 export default function MatrixComp() {
+  const testData = {
+    img_src: testIcon,
+    name: "Terri",
+    location: "Phoenix, AZ",
+    blurb:
+      "Terri's great life suddenly vanished when her job disappeared and she needed help from the food pantry at her local church. Now that she's back on her feet, she's a regular volunteer at the pantry that helped stabilize her life.",
+  };
   return (
     <div>
       <p> Matrix Component </p>
-      <IconMatrix />
+      <IconMatrix data={testData} />
     </div>
   );
 }
