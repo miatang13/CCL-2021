c9e851da7b59863dbb69dad8f870d8fd6cd1a08b
miatang13 <miatang13@gmail.com>
2021-11-10 17:42:06 -0500

    Story with caption

--- Food Insecurity Project/food-insecurity/src/pages/stories/Terri.js
+++ Food Insecurity Project/food-insecurity/src/pages/stories/Terri.js
@@ -1,76 +1,32 @@
 import Button from "@restart/ui/esm/Button";
-import { Col, Container, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import {
-  header_img_base_url,
-  icon_img_base_url,
-  story_img_base_url,
-} from "../../data/baseUrls";
+import { story_img_base_url } from "../../data/baseUrls";
 import story_data from "../../data/storyInfo.json";
-import "../../styles/story.css";
-import "../../styles/mouseMask.css";
 import { useEffect, useRef, useState } from "react";
 import classNames from "classnames";
+import "../../styles/story.css";
+import "../../styles/mouseMask.css";
 
 export default function TerriPage() {
+  /**
+   * Data
+   */
   const data = story_data["Terri"];
-  /*
-  return (
-    <Container className="story__container">
-      <div className="d-flex justify-content-center header__img__container">
-        {" "}
-        <img
-          src={header_img_base_url + data.header_img_url}
-          alt={data.name + "header image"}
-          width="85%"
-        ></img>
-      </div>
-      <Link to="/matrix">
-        {" "}
-        <Button>Back to matrix </Button>
-      </Link>
-      <div className="content__container">
-        <Row>
-          <Col xs={3}>
-            <h2> Biography </h2>
-            <img
-              src={icon_img_base_url + data.icon_img_url}
-              alt={data.name + "icon image"}
-            />
-          </Col>
-          <Col>
-            <h1> {data.name} </h1>
-            <p>{data.intro}</p>{" "}
-          </Col>
-        </Row>
-        <section></section>
-        <section>
-          <h2> Behind the numbers </h2>
-          {data.content.map((cont, i) => {
-            return (
-              <div className="indiv__content__wrapper" key={i}>
-                <p className="data__text"> {cont.data_content}</p>
-                <p className="narrative_text"> {cont.narrative_content}</p>
-              </div>
-            );
-          })}
-        </section>
-      </div>
-    </Container>
-  ); */
+  const [contentText, setContentText] = useState(data.intro);
 
-  var bgRef = useRef();
-  var timer = useRef(0);
-  var mouseX = useRef(0);
-  var mouseY = useRef(0);
-  var xp = useRef(0);
-  var yp = useRef(0);
-  var circleRef = useRef();
+  /**
+   * Interaction
+   */
+  const bgRef = useRef();
+  const timer = useRef(0);
+  const mouseX = useRef(0);
+  const mouseY = useRef(0);
+  const xp = useRef(0);
+  const yp = useRef(0);
+  const circleRef = useRef();
   const [isMoving, setMoving] = useState(false);
 
   function mouseStopped() {
-    // if mouse stop moving remove class moving
-    // it will hide the circle with opacity transition
     setMoving(false);
   }
 
@@ -85,7 +41,6 @@ export default function TerriPage() {
 
     document.addEventListener("mousemove", onMouseMove);
 
-    // set the momentum with setInterval function
     const interval = setInterval(function () {
       const damp = 10;
       xp.current += (mouseX.current - xp.current) / damp;
@@ -101,21 +56,33 @@ export default function TerriPage() {
   }, []);
 
   return (
-    <div class="wrapper full-size">
-      <div class="mask-bg-color full-size"></div>
-      <div class="blend-multiply full-size">
-        <img
-          class="bg full-size"
-          ref={bgRef}
-          src={story_img_base_url + "test.png"}
-          alt="background"
-        />
-        <div class="blend-screen element-mask full-size">
-          <span
-            id="circle"
-            className={classNames("circle-follow", { moving: isMoving })}
-            ref={circleRef}
-          ></span>
+    <div>
+      <Link to="/matrix">
+        {" "}
+        <Button>Back to matrix </Button>
+      </Link>
+      <div class="wrapper">
+        <div class="mask-bg-color full-size"></div>
+        <div class="blend-multiply full-size">
+          <img
+            class="bg full-size"
+            ref={bgRef}
+            src={story_img_base_url + "test.png"}
+            alt="background"
+          />
+          <div class="blend-screen element-mask full-size">
+            <span
+              id="circle"
+              className={classNames("circle-follow", { moving: isMoving })}
+              ref={circleRef}
+            ></span>
+          </div>
+        </div>
+      </div>
+      <div id="content__container">
+        <div id="content__text">
+          <h1> {data.name} </h1>
+          <p> {contentText}</p>
         </div>
       </div>
     </div>
