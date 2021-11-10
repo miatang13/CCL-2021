67d8c08eea504d4a9c75c21187f58788bf48553e
miatang13 <miatang13@gmail.com>
2021-11-09 21:15:20 -0500

    Dynamic load image from json

--- Food Insecurity Project/food-insecurity/src/components/IconMatrix.js
+++ Food Insecurity Project/food-insecurity/src/components/IconMatrix.js
@@ -4,25 +4,25 @@ import { useRef, useState } from "react";
 import { OverlayTrigger } from "react-bootstrap";
 
 export default function IconMatrix(iconProp) {
+  console.log(iconProp);
   const [show, setShow] = useState(false);
   const target = useRef(null);
-
   return (
     <>
       <img
         ref={target}
         onClick={() => setShow(!show)}
         className="matrix__icon__img"
-        src={iconProp.data.img_src}
+        src={iconProp.img_src} //{require(`${iconProp.img_src}`)}
         alt="icon"
         delay={{ show: 700, hide: 400 }}
       />
       <Overlay target={target.current} show={show} placement="right">
         {({ placement, arrowProps, show: _show, popper, ...props }) => (
           <div {...props} className="icon__info">
-            <span>{iconProp.data.name} </span>
-            <span>{iconProp.data.location} </span>
-            <p> {iconProp.data.blurb} </p>
+            <span>{iconProp.name} </span>
+            <span>{iconProp.location} </span>
+            <p> {iconProp.blurb} </p>
           </div>
         )}
       </Overlay>
--- Food Insecurity Project/food-insecurity/src/components/MatrixQuadrant.js
+++ Food Insecurity Project/food-insecurity/src/components/MatrixQuadrant.js
@@ -1,12 +1,20 @@
-import { icon_info } from "../data/iconInfo";
+import icon_data from "../data/iconInfo.json";
 import IconMatrix from "./IconMatrix";
 
 export default function MatrixQuadrant() {
+  const baseUrl = "/assets/img/matrixIcon/";
+
   return (
     <div>
       <p> Matrix Component </p>
-      {icon_info.map((indivData, i) => (
-        <IconMatrix data={indivData} key={i} />
+      {icon_data.map((indivData, i) => (
+        <IconMatrix
+          name={indivData.name}
+          location={indivData.location}
+          img_src={baseUrl + indivData.img_src}
+          blurb={indivData.blurb}
+          key={i}
+        />
       ))}
     </div>
   );
